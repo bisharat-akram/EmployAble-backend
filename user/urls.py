0b0d51bdb8d875076f5b7ad8866bba43a969edb1
bisharat-akram <bisharat.akram@zweidevs.com>
2023-09-11 15:57:37 +0500

    Add Google login

--- user/urls.py
+++ user/urls.py
@@ -4,6 +4,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView
 
 urlpatterns = [
     path('signup/', UserSignUPView.as_view(), name="signup_for_a_user"),
+    path('google-login/', GoogleLoginView.as_view(), name="signup_for_a_user"),
     path('login/', TokenObtainPairView.as_view(), name="get_token_with_email_password"),
     path('me', GetUserView.as_view(), name="get_current_user_data"),
 ]
--- user/views.py
+++ user/views.py
@@ -4,8 +4,8 @@ from rest_framework.response import Response
 from rest_framework.exceptions import ValidationError
 from .serializers import UserSerializer
 from .models import User
-import pdb
-
+import requests
+from rest_framework_simplejwt.tokens import RefreshToken
 
 # Create your views here.
 class UserSignUPView(APIView):
@@ -38,3 +38,48 @@ class GetUserView(APIView):
     def get(self, request):
         """ method to get user data """
         return Response(UserSerializer(request.user).data)
+    
+
+class GoogleLoginView(APIView):
+    """ View to create google login accounts in database """
+    permission_classes = (AllowAny,)
+
+    def post(self, request):
+        """ Method to create google users in database """
+        google_access_token = request.data['access_token']
+        google_user_info_url = "https://people.googleapis.com/v1/people/me?personFields=names,emailAddresses,birthdays"
+        headers = {'Authorization': f'Bearer {google_access_token}'}
+        try:
+            response = requests.get(google_user_info_url, headers = headers)
+            if response.status_code == 200:
+                response = response.json()
+                email = response['emailAddresses'][0]['value']
+                if User.objects.filter(email = email).exists():
+                    user_obj = User.objects.get(email = email)
+                    refresh_token = RefreshToken.for_user(user_obj)
+                    return Response({
+                        "access": str(refresh_token.access_token),
+                        "refresh": str(refresh_token)
+                    })
+                else:
+                    user_obj = User(
+                        email = response['emailAddresses'][0]['value'],
+                        first_name = response['names'][0]['givenName'],
+                        last_name = response['names'][0]['familyName'],
+                        password = "createdByGoogleAccount",
+                        user_type = 1,
+                        username = response['emailAddresses'][0]['value'],
+                        auth_type = 1
+                    )
+                    user_obj.save()
+                    refresh_token = RefreshToken.for_user(user_obj)
+                    return Response({
+                        "access": str(refresh_token.access_token),
+                        "refresh": str(refresh_token)
+                    })
+            else:
+                raise ValidationError(response.json())
+        except Exception as e:
+            raise ValidationError(e)
+
+
