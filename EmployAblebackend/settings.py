cc8209ba042ed9a61a2b1b28d4a6a0e547e2321c
bisharat-akram <bisharat.akram@zweidevs.com>
2023-10-08 18:25:42 +0500

    Add Cors

--- EmployAblebackend/settings.py
+++ EmployAblebackend/settings.py
@@ -63,6 +63,7 @@ DRF_STANDARDIZED_ERRORS = {"EXCEPTION_FORMATTER_CLASS": "configs.exception_handl
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
+    "https://main.d21g61v83td32q.amplifyapp.com"
 ]
 
 MIDDLEWARE = [
